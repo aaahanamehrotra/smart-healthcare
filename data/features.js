47be3830224903c81c68c47079915424e0cf8016
Aaahana Mehrotra <83857184+aaahanamehrotra@users.noreply.github.com>
2023-05-01 22:59:17 +0530

    add features

--- data/features.js
+++ data/features.js
@@ -2,19 +2,19 @@ const data = [
   {
     number: "1",
     title: "Lorem Ipsum",
-    desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Porro illum voluptate molestias est ex autem modi tempore, praesentium incidunt odit doloribus unde? Ad qui tenetur possimus error aut porro maxime.",
+    desc: "to create a sophisticated version of biological lungs by combining an external artificial respiratory system with a genetically engineered one.",
   },
 
   {
     number: "2",
     title: "Lorem Ipsum",
-    desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Porro illum voluptate molestias est ex autem modi tempore, praesentium incidunt odit doloribus unde? Ad qui tenetur possimus error aut porro maxime.",
+    desc: " to develop a model that analyses CT scans  and X Rays and predicts if the infected patients show signs of pneumonia. ",
   },
 
   {
     number: "3",
     title: "Lorem Ipsum",
-    desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Porro illum voluptate molestias est ex autem modi tempore, praesentium incidunt odit doloribus unde? Ad qui tenetur possimus error aut porro maxime.",
+    desc: "to analyze and visually present large amounts of COVID-19 data using appealing graphics. With these objectives, we aim to have a wide-ranging impact.",
   },
 
   {
