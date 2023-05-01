9508a14a921c40d64c90044a6073cef447f44549
Aaahana Mehrotra <83857184+aaahanamehrotra@users.noreply.github.com>
2023-05-01 23:10:08 +0530

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,11 +7,10 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div className={styles.main}>
-      <h1 className={styles.heading}>Lorem Ipsum</h1>
+      <h1 className={styles.heading}>Biological Lungs</h1>
       <hr />
       <h3 className={styles.tagline}>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Error vero
-        nemo, illum deserunt laboriosam saepe fugit.
+        Your Health, Our Priority
       </h3>
       <div className={styles.buttons}>
         <Link href={"/features"}>
