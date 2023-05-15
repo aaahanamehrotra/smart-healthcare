64046beb67e0b156ab99b157961a300f99fa5a2c
aaahanamehrotra <e10991aaahana@dpsrkp.net>
2023-05-15 21:42:07 +0530

    add footer

---
+++ src/components/Footer.js
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "../styles/Footer.module.css";
+
+const Feature = ({ number, title, desc }) => {
+  return <div className={styles.footer}>PulmoGen 2023</div>;
+};
+
+export default Feature;
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,10 +1,16 @@
 import "@/styles/globals.css";
+import Head from "next/head";
 import Nav from "@/components/Nav";
+import Footer from "@/components/Footer";
 export default function App({ Component, pageProps }) {
   return (
     <>
+      <Head>
+        <title>PulmoGen</title>
+      </Head>
       <Nav />
       <Component {...pageProps} />
+      <Footer />
     </>
   );
 }
